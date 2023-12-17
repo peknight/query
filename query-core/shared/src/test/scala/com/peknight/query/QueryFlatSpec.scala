2a52b094d727441a8092bc6827c1dbaf9c770d31
peknight <JKpeknight@gmail.com>
2023-12-17 22:34:45 +0800

    update

--- query-core/shared/src/test/scala/com/peknight/query/QueryFlatSpec.scala
+++ query-core/shared/src/test/scala/com/peknight/query/QueryFlatSpec.scala
@@ -12,30 +12,14 @@ import com.peknight.generic.migration.id.Migration
 
 class QueryFlatSpec extends AnyFlatSpec:
   "Query Codec" should "succeed" in {
-    given configuration: CodecConfiguration = CodecConfiguration()
+    val configuration: CodecConfiguration = CodecConfiguration()
     val iceCream = IceCream("rua", 7, true)
     val tuple = Tuple.fromProductTyped(iceCream)
-
-    val monad: Monad[Id] = Monad[Id]
-    println(monad)
-    val cursorType: CursorType.Aux[Cursor[Query], Query] = CursorType[Cursor[Query]].asInstanceOf
-    println(cursorType)
-    val objectType: ObjectType[Query] = ObjectType[Query]
-    println(objectType)
-    val nullType: NullType[Query] = NullType[Query]
-    println(nullType)
-    val failure: Migration[DecodingFailure, DecodingFailure] = summon[Migration[DecodingFailure, DecodingFailure]]
-    println(failure)
-    val stringDecoder: Decoder[String] = Decoder[String]
-    println(stringDecoder)
-    val stringOptionDecoder: Decoder[Option[String]] = Decoder[Option[String]]
-    println(stringOptionDecoder)
-    val instances: Generic.Instances[Decoder, IceCream] = Generic.Product.Instances[Decoder, IceCream]
-    println(instances)
-    // val decoder: Decoder[IceCream] =
-    //   com.peknight.codec.Decoder.derived[Id, Query, Cursor[Query], DecodingFailure, IceCream](using configuration)(using
-    //     monad, cursorType, objectType, nullType, failure, stringDecoder, stringOptionDecoder, instances
-    //   )
-    // println(decoder)
+    val encoder: Encoder[IceCream] = com.peknight.codec.Encoder.derived(using configuration)
+    val decoder: Decoder[IceCream] = com.peknight.codec.Decoder.derived(using configuration)
+    val tupleEncoder: Encoder[(String, Int, Boolean)] = Encoder[(String, Int, Boolean)]
+    val tupleDecoder: Decoder[(String, Int, Boolean)] = Decoder[(String, Int, Boolean)]
+    println(decoder.decodeS(encoder.encode(iceCream)))
+    println(tupleDecoder.decodeS(tupleEncoder.encode(tuple)))
   }
 end QueryFlatSpec
