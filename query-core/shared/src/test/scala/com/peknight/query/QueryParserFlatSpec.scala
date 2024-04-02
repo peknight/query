7ade06004d41a7d6ef3013dc62bfe509eaeb0d6f
peknight <JKpeknight@gmail.com>
2024-04-02 17:11:41 +0800

    update

--- query-core/shared/src/test/scala/com/peknight/query/QueryParserFlatSpec.scala
+++ query-core/shared/src/test/scala/com/peknight/query/QueryParserFlatSpec.scala
@@ -16,16 +16,6 @@ class QueryParserFlatSpec extends AnyFlatSpec:
   }
 
   "Query Parser" should "succeed with company" in {
-    import cats.Id
-    import com.peknight.codec.Decoder.{decodeList, derived}
-    import com.peknight.codec.cursor.id.Decoder
-    given fruitDecoder: Decoder[Query, Fruit] = derived[Id, Query, Fruit]
-    given fruitsDecoder: Decoder[Query, List[Fruit]] = decodeList[Id, Query, Fruit]
-    given employeeDecoder: Decoder[Query, Employee] = derived[Id, Query, Employee]
-    given employeesDecoder: Decoder[Query, List[Employee]] = decodeList[Id, Query, Employee]
-    given departmentDecoder: Decoder[Query, Department] = derived[Id, Query, Department]
-    given departmentsDecoder: Decoder[Query, List[Department]] = decodeList[Id, Query, Department]
-    given companyDecoder: Decoder[Query, Company] = derived[Id, Query, Company]
     val company = Company("Pek", List(
       Department("X", List(Employee("A", 20, List(Fruit.Apple, Fruit.Pear)), Employee("B", 25, List(Fruit.Peach)))),
       Department("Y", List(Employee("C", 30, List(Fruit.Apple, Fruit.Peach)), Employee("D", 35, List(Fruit.Pear))))
